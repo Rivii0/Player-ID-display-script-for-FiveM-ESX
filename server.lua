26d9c906027e6817b1300cae89683e6e34e23a7e
Rivii <168650921+Rivii0@users.noreply.github.com>
2025-01-19 20:22:41 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,4 @@
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+ESX = exports["es_extended"]:getSharedObject()
 
 RegisterServerEvent('logPlayerAction')
 AddEventHandler('logPlayerAction', function(playerId, action)
